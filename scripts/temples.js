3d661e36dab02fcb1275c53816d109536bb953aa
ninafuentes <nfuentesguerrero@byupathway.edu>
2026-01-24 17:15:19 -0700

    v3 changes 3

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,1 @@
-const menuButton = document.getElementById("menuButton");
-const navMenu = document.getElementById("navMenu");
 
-menuButton.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
-  menuButton.textContent = navMenu.classList.contains("show") ? "✖" : "☰";
-});
-
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
