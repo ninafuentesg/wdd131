c89a0c50c285f5191073aabb302534bcc51a1315
ninafuentes <nfuentesguerrero@byupathway.edu>
2026-01-31 14:13:49 -0700

    w3 changes

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+// static values
+const temp = 45; // Fahrenheit
+const windSpeed = 8; // mph
+
+function calculateWindChill(tempF, windMph) {
+    if(tempF <= 50 && windMph > 3){
+        return Math.round(35.74 + 0.6215*tempF - 35.75*Math.pow(windMph,0.16) + 0.4275*tempF*Math.pow(windMph,0.16)) + "°F";
+    } else {
+        return "N/A";
+    }
+}
+
+// Display values
+document.getElementById("temp").textContent = temp;
+document.getElementById("wind").textContent = windSpeed;
+document.getElementById("windchill").textContent = calculateWindChill(temp, windSpeed);
+
+// Footer dates
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastmod").textContent = document.lastModified;
